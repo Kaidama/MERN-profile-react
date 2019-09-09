512fa7a68cd171be45e52e0b14a7dc50c7dda680
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-09-09 14:37:28 -0400

    5. users post route, test passed with postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
-
+//@desc test user route
+//@route GET api/users
 router.get('/', (req, res) => {
     res.send('Users Route')
+})
+//@desc Register User
+//@routes POST api/users
+router.post('/', (req, res) => {
+    console.log(`: `, req.body);
+    
+    res.send('User Route')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 const express = require("express");
-const mongoose = require('mongoose')
-const path = require('path')
-require('dotenv').config()
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
 // connect to MONGODB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-.then( result => console.log(`Connected to MONGODB ATLAS: `, result.connections)
-)
-.catch(error => console.log(`error: `, error)
-)
-app.get("/", (req, res) => res.send("API poop"));
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(result =>
+    console.log(`Connected to MONGODB ATLAS: `, result.connections)
+  )
+  .catch(error => console.log(`error: `, error));
 
-//routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/post', require('./routes/api/post'))
+  app.get("/", (req, res) => res.send("API poop"));
 
+//INITIALLZE MIDDLEWARE
+//replaces bodyparser because express comes with one already
+app.use(express.json({ extended: false }));
 
-
-
+//API ROUTES
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
-
-
- 
